fbbcf3a76408dc07f3ddebc9eaa21479e26c717e
andy0621 <306666416@qq.com>
2021-03-24 23:26:23 +0800

    Update centos7-init.sh

--- centos7-init.sh
+++ centos7-init.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-echo "正在更新系统版本......"
+echo -e "\033[41;37m正在更新系统版本......\033[0m"
 yum update -y
 
-echo "正在安装常用工具......"
+echo -e "\033[41;37m正在安装常用工具......\033[0m"
 yum install -y vim* net-tools unzip wget yum-utils telnet xinetd git
 
-echo "正在关闭防火墙........"
+echo -e "\033[41;37m正在关闭防火墙........\033[0m"
 systemctl stop firewalld
 systemctl disable firewalld.service
 firewall-cmd --state
 
-echo "CentOS7初始化完成....."
+echo -e "\033[41;37mCentOS7初始化完成.....\033[0m"
 
-echo "系统信息如下.........."
-echo "主机名："`hostname`
+echo -e "\033[41;37m系统信息如下..........\033[0m"
+echo -e "\033[41;37m主机名："`hostname`"\033[0m"
 
-echo "IP地址："`ifconfig | grep broadcast | awk "NR==1" | awk -F" " '{print $2}'`
+echo -e "\033[41;37mIP地址："`ifconfig | grep broadcast | awk "NR==1" | awk -F" " '{print $2}'`"\033[0m"
 exit 0
